f1fa80bb8bd212a1c77d9baddec8310adb7bb939
Anoushka Sawant <anoushka@umail.ucsb.edu>
2023-05-22 13:08:41 -0700

    frontend hopefully finished

---
+++ frontend/src/fixtures/movieFixtures.js
@@ -0,0 +1,38 @@
+const movieFixtures = {
+    oneMovie:
+    [
+      {
+       "id": 1,
+        "name": "John Wick",
+        "starring": "Keanu Reeves",
+        "director": "Chad Stahelski",
+      }
+    ],
+
+    threeMovies:
+    [
+        {
+            "id": 2,
+            "name": "San Andreas",
+            "starring": "Dwayne Johnson",
+            "director": "Brad Peyton",
+        },
+
+        {
+            "id": 3,
+            "name": "Inception",
+            "starring": "Leonardo DiCaprio",
+            "director": "Christopher Nolan",
+        },
+
+        {
+            "id": 4,
+            "name": "Forrest Gump",
+            "starring": "Tom Hanks",
+            "director": "Robert Zemeckis",
+        },
+        
+    ]
+};
+
+export { movieFixtures };
