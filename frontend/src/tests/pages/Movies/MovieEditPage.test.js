d46fa9e59f3adac7d20ee1fc26c3c8607f710185
Anoushka Sawant <anoushka@umail.ucsb.edu>
2023-05-22 13:22:58 -0700

    frontend should be done

--- frontend/src/tests/pages/Movies/MovieEditPage.test.js
+++ frontend/src/tests/pages/Movies/MovieEditPage.test.js
@@ -3,7 +3,10 @@ import MovieEditPage from "main/pages/Movies/MovieEditPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import mockConsole from "jest-mock-console";
-
+import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
 const mockNavigate = jest.fn();
 
 jest.mock('react-router-dom', () => ({
@@ -36,7 +39,9 @@ jest.mock('main/utils/movieUtils', () => {
 
 
 describe("MovieEditPage tests", () => {
-
+    const axiosMock =new AxiosMockAdapter(axios);
+    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither); 
     const queryClient = new QueryClient();
 
     test("renders without crashing", () => {
