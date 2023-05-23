d78a941d370dc9de188e367239ff60b0cf81c5d3
MrJacobEisner <mrjeisner@gmail.com>
2023-05-22 17:32:24 -0700

    Completed Storybook pages

---
+++ frontend/src/stories/pages/Songs/SongDetailsPage.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import SongDetailsPage from "main/pages/Songs/SongDetailsPage";
+
+export default {
+    title: "pages/Songs/SongDetailsPage",
+    component: SongDetailsPage,
+};
+
+const Template = () => <SongDetailsPage />;
+
+export const Default = Template.bind({});
---
+++ frontend/src/stories/pages/Songs/SongEditPage.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import SongEditPage from "main/pages/Songs/SongEditPage";
+
+export default {
+    title: "pages/Songs/SongEditPage",
+    component: SongEditPage,
+};
+
+const Template = () => <SongEditPage />;
+
+export const Default = Template.bind({});
